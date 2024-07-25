742fe5138915f12f8426a12e4fbed99ca132fda6
Gayashan <gayazhanmit@yahoo.com>
2024-07-25 13:33:32 -0700

    backend boiler plate project added

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+// importing packages
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+// require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+
+// setups
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// define Schema Class
+import items from './routes/items.js'
+
+// Mount the router middleware at a specific path
+app.use('/api', items);
+
+// Connect to MongoDB
+const URI = process.env.ATLAS_URI;
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+        // Start your Express server once connected to MongoDB
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
