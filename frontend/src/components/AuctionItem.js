6e9eb8ff0bd133da6c12277490d372b9298f8699
Gayashan <gayazhanmit@yahoo.com>
2024-08-06 11:23:49 -0700

    fixed image relative path, integrated deployed backend

--- frontend/src/components/AuctionItem.js
+++ frontend/src/components/AuctionItem.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function AuctionItem({ id, title, description, image }) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} />
+      <Card.Img variant="top" src={ `${process.env.PUBLIC_URL}/images/${ image }` } />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{description}</Card.Text>
